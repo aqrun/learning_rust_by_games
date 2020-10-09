a22d1ad70c1a1a686b80774bedd0a002905e9aa4
Aqrun <aqrun@sina.com>
2020-10-09 21:30:19 +0800

    feat: show a more complex plane

---
+++ src/bin/1-2-3-complex-plane.rs
@@ -0,0 +1,43 @@
+extern crate aqrun_lrbg;
+
+use aqrun_lrbg::common;
+use crossterm::event::{
+    self, Event, KeyCode, KeyEvent
+};
+
+/// show a more complex plane
+pub fn run() {
+    let mut x = 10;
+    let mut y = 5;
+
+    loop {
+        common::clear();
+
+        for _ in 0..y {
+            println!("");
+        }
+
+        for _ in 0..x {
+            print!(" ");
+        }
+        println!("  *");
+        for _ in 0..x {
+            print!(" ");
+        }
+        println!("*****");
+        for _ in 0..x {
+            print!(" ");
+        }
+        println!(" * * ");
+
+        if let Event::Key(KeyEvent{ code, .. }) = event::read().unwrap() {
+            match code {
+                KeyCode::Char('a') => x -= 1,
+                KeyCode::Char('w') => y -= 1,
+                KeyCode::Char('s') => y += 1,
+                KeyCode::Char('d') => x += 1,
+                _ => {}
+            }
+        }
+    }
+}
