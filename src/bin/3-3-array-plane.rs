5eea6a01b3276231bc68cc87224aa37bd569c92e
Aqrun <aqrun@sina.com>
2020-10-12 23:31:14 +0800

    feat: 显示多台敌机

--- src/bin/3-3-array-plane.rs
+++ src/bin/3-3-array-plane.rs
@@ -13,6 +13,7 @@ use rand::prelude::*;
 
 const HIGH: usize = 25;
 const WIDTH: usize = 50;
+const ENEMY_NUM: i32 = 5;
 
 /// 空战游戏
 /// 击中敌机
@@ -20,11 +21,10 @@ struct App {
     pub position_x: i32,
     pub position_y: i32,
     pub canvas: Vec<Vec<i32>>,
-    pub enemy_x: i32,
-    pub enemy_y: i32,
+    pub enemy_x: Vec<usize>,
+    pub enemy_y: Vec<usize>,
 
     pub score: i32,
-
     pub speed: i32,
 }
 
@@ -32,19 +32,25 @@ impl App {
     pub fn new() -> Self {
         let position_x = WIDTH / 2;
         let position_y = HIGH / 2;
-        let enemy_x = position_x;
-        let enemy_y = 0;
+
+        let mut enemy_x = vec![0_usize; ENEMY_NUM as usize];
+        let mut enemy_y = vec![0_usize; ENEMY_NUM as usize];
 
         let mut canvas = vec![vec![0; WIDTH]; HIGH];
         canvas[position_y][position_x] = 1;
-        canvas[enemy_y][enemy_x] = 3;
+        
+        for i in 0..ENEMY_NUM {
+            enemy_x[i as usize] = thread_rng().gen_range(0, WIDTH);
+            enemy_y[i as usize] = thread_rng().gen_range(0, 2_usize);
+            canvas[enemy_y[i as usize]][enemy_x[i as usize]] = 3;
+        }
 
         App {
             canvas,
             position_x: position_x as i32,
             position_y: position_y as i32,
-            enemy_x: enemy_x as i32,
-            enemy_y: enemy_y as i32,
+            enemy_x,
+            enemy_y,
 
             score: 0,
             speed: 0,
@@ -80,53 +86,62 @@ impl App {
     }
 
     pub fn update_without_input(&mut self) -> Result<String, String> {
-        for i in 0..(HIGH as i32) {
-            for j in 0..(WIDTH as i32) {
-                if self.canvas[i as usize][j as usize] == 2 {
-                    // 子弹击中敌机
-                    if (i == self.enemy_y) && (j == self.enemy_x) {
-                        self.score += 1;
-                        self.set_pos_val(self.enemy_x, self.enemy_y, 0);
-                        self.enemy_y = 0;
-                        self.enemy_x = thread_rng().gen_range(0, WIDTH as i32);
-                        self.set_pos_val(self.enemy_x, self.enemy_y, 0);
-                        self.set_pos_val(j, i, 0);
+        for i in 0..HIGH {
+            for j in 0..WIDTH {
+                if self.canvas[i][j] == 2 {
+                    for k in 0..(ENEMY_NUM as usize) {
+                        // 子弹击中敌机
+                        if (i == self.enemy_y[k]) && (j == self.enemy_x[k]) {
+                            self.score += 1;
+                            self.set_usize_val(self.enemy_x[k], self.enemy_y[k], 0);
+                            self.enemy_y[k] = thread_rng().gen_range(0, 2_usize);
+                            self.enemy_x[k] = thread_rng().gen_range(0, WIDTH);
+                            self.set_usize_val(self.enemy_x[k], self.enemy_y[k], 0);
+                            self.set_usize_val(j, i, 0);
+                        }
                     }
 
                     // 子弹向上移动
-                    self.set_pos_val(j, i, 0);
+                    self.set_usize_val(j, i, 0);
                     if i > 0 {
-                        self.set_pos_val(j, i - 1, 2);
+                        self.set_usize_val(j, i - 1, 2);
                     }
                 }
             }
-        }
-
-        // 检测敌机撞到我机
-        if (self.position_x == self.enemy_x) && (self.position_y == self.enemy_y) {
-            return Err(String::from("game over"));
-        }
-
-        // 敌机跑出显示屏
-        if self.enemy_y > HIGH as i32 {
-            self.set_pos_val(self.enemy_x, self.enemy_y, 0);
-            self.enemy_y = 0;
-            self.enemy_x = thread_rng().gen_range(0, WIDTH as i32);
-            self.set_pos_val(self.enemy_x, self.enemy_y, 3);
-            self.score -= 1;
         }
 
         if self.speed < 10 {
             self.speed += 1;
         }
 
-        if self.speed >= 10 {
-            self.set_pos_val(self.enemy_x, self.enemy_y, 0);
-            self.enemy_y += 1;
-            self.speed = 0;
-            self.set_pos_val(self.enemy_x, self.enemy_y, 3);
-        }
+        for kk in 0..ENEMY_NUM {
+            let k = kk as usize;
+            // 检测敌机撞到我机
+            if (self.position_x == self.enemy_x[k] as i32) 
+                && (self.position_y == self.enemy_y[k] as i32
+            ) {
+                return Err(String::from("game over"));
+            }
 
+            // 敌机跑出显示屏
+            if self.enemy_y[k] > HIGH {
+                self.set_usize_val(self.enemy_x[k], self.enemy_y[k], 0);
+                self.enemy_y[k] = thread_rng().gen_range(0, 2_usize);
+                self.enemy_x[k] = thread_rng().gen_range(0, WIDTH);
+                self.set_usize_val(self.enemy_x[k], self.enemy_y[k], 3);
+                self.score -= 1;
+            }
+
+            if self.speed >= 10 {
+                for m in 0..(ENEMY_NUM as usize) {
+                    self.set_usize_val(self.enemy_x[m], self.enemy_y[m], 0);
+                    self.enemy_y[m] += 1;
+                    self.speed = 0;
+                    self.set_usize_val(self.enemy_x[m], self.enemy_y[m], 3);
+                }
+            }
+        }
+        
         thread::sleep(Duration::from_millis(150));
         Ok(String::from(""))
     }
@@ -186,7 +201,23 @@ impl App {
     }
 
     pub fn set_pos_val(&mut self, pos_x: i32, pos_y: i32, val: i32) {
-        self.canvas[pos_y as usize][pos_x as usize] = val;
+        let mut x = pos_x;
+        let mut y = pos_y;
+        let w = WIDTH as i32;
+        let h = HIGH as i32;
+
+        if x >= w - 1 {
+            x = w - 1;
+        }
+
+        if y >= h - 1 {
+            y = h - 2;
+        }
+        self.canvas[y as usize][x as usize] = val;
+    }
+
+    pub fn set_usize_val(&mut self, pos_x: usize, pos_y: usize, val: i32) {
+        self.canvas[pos_y][pos_x] = val;
     }
 }
 
