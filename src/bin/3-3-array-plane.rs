8461dc7e23078d4ade8cca379989f0a5d4e822c8
Aqrun <aqrun@sina.com>
2020-10-12 22:53:32 +0800

    feat: 击中敌机

--- src/bin/3-3-array-plane.rs
+++ src/bin/3-3-array-plane.rs
@@ -15,27 +15,39 @@ const HIGH: usize = 25;
 const WIDTH: usize = 50;
 
 /// 空战游戏
+/// 击中敌机
 struct App {
     pub position_x: i32,
     pub position_y: i32,
     pub canvas: Vec<Vec<i32>>,
+    pub enemy_x: i32,
+    pub enemy_y: i32,
 
     pub score: i32,
+
+    pub speed: i32,
 }
 
 impl App {
     pub fn new() -> Self {
         let position_x = WIDTH / 2;
         let position_y = HIGH / 2;
+        let enemy_x = position_x;
+        let enemy_y = 0;
+
         let mut canvas = vec![vec![0; WIDTH]; HIGH];
         canvas[position_y][position_x] = 1;
+        canvas[enemy_y][enemy_x] = 3;
 
         App {
             canvas,
             position_x: position_x as i32,
             position_y: position_y as i32,
+            enemy_x: enemy_x as i32,
+            enemy_y: enemy_y as i32,
 
             score: 0,
+            speed: 0,
         }
     }
 
@@ -53,6 +65,8 @@ impl App {
                    print!("*");
                 } else if self.canvas[i][j] == 2 {
                     print!("|");
+                } else if self.canvas[i][j] == 3 {
+                    print!("@");
                 }
             }
             println!("|");
@@ -66,15 +80,51 @@ impl App {
     }
 
     pub fn update_without_input(&mut self) -> Result<String, String> {
-        for i in 0..HIGH {
-            for j in 0..WIDTH {
-                if self.canvas[i][j] == 2 {
-                    self.canvas[i][j] = 0;
+        for i in 0..(HIGH as i32) {
+            for j in 0..(WIDTH as i32) {
+                if self.canvas[i as usize][j as usize] == 2 {
+                    // 子弹击中敌机
+                    if (i == self.enemy_y) && (j == self.enemy_x) {
+                        self.score += 1;
+                        self.set_pos_val(self.enemy_x, self.enemy_y, 0);
+                        self.enemy_y = 0;
+                        self.enemy_x = thread_rng().gen_range(0, WIDTH as i32);
+                        self.set_pos_val(self.enemy_x, self.enemy_y, 0);
+                        self.set_pos_val(j, i, 0);
+                    }
+
+                    // 子弹向上移动
+                    self.set_pos_val(j, i, 0);
                     if i > 0 {
-                        self.set_pos_val(i - 1, j, 2);
+                        self.set_pos_val(j, i - 1, 2);
                     }
                 }
             }
+        }
+
+        // 检测敌机撞到我机
+        if (self.position_x == self.enemy_x) && (self.position_y == self.enemy_y) {
+            return Err(String::from("game over"));
+        }
+
+        // 敌机跑出显示屏
+        if self.enemy_y > HIGH as i32 {
+            self.set_pos_val(self.enemy_x, self.enemy_y, 0);
+            self.enemy_y = 0;
+            self.enemy_x = thread_rng().gen_range(0, WIDTH as i32);
+            self.set_pos_val(self.enemy_x, self.enemy_y, 3);
+            self.score -= 1;
+        }
+
+        if self.speed < 10 {
+            self.speed += 1;
+        }
+
+        if self.speed >= 10 {
+            self.set_pos_val(self.enemy_x, self.enemy_y, 0);
+            self.enemy_y += 1;
+            self.speed = 0;
+            self.set_pos_val(self.enemy_x, self.enemy_y, 3);
         }
 
         thread::sleep(Duration::from_millis(150));
@@ -107,7 +157,7 @@ impl App {
                         self.set_canvas(1);
                     },
                     KeyCode::Char(' ') => {
-                        self.set_pos_val(self.position_y - 1, self.position_x, 2);
+                        self.set_pos_val(self.position_x, self.position_y - 1, 2);
                     },
                     KeyCode::Esc => {
                         return Ok("Exit");
@@ -120,7 +170,19 @@ impl App {
     }
 
     pub fn set_canvas(&mut self, val: i32) {
-        self.canvas[self.position_y as usize][self.position_x as usize] = val;
+        let mut x = self.position_x;
+        let mut y = self.position_y;
+        let w = WIDTH as i32;
+        let h = HIGH as i32;
+
+        if x >= w - 1 {
+            x = w - 1;
+        }
+
+        if y >= h - 1 {
+            y = h - 1;
+        }
+        self.canvas[y as usize][x as usize] = val;
     }
 
     pub fn set_pos_val(&mut self, pos_x: i32, pos_y: i32, val: i32) {
@@ -167,7 +229,5 @@ pub fn main() {
     ).unwrap();
     terminal::disable_raw_mode().unwrap();
 
-    if let Err(_) = run_res {
-        println!("得分：{}", app.score);
-    }
+    println!("得分：{}", app.score);
 }
